6346f9a5b257fd80881966fd6e8689d9adb1c94c
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-05-10 17:07:49 +0200

    💬 ajout du composant navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar/Navbar";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React - 1
-        </a>
-      </header>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Hello World
+      </h1>
     </div>
-  );
+  )
 }
-
-export default App;
