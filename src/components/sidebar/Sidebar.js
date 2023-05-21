a26673d85e1176a606fca312d5e1d51a55d3aa4b
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-05-21 02:05:40 +0200

    :bug: Implémentation des séries

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,88 +1,105 @@
-import React, { useEffect, useState } from "react";
-import { faChessRook, faGun } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { series_service } from "../../services/series.service";
+import React, {useEffect, useState} from "react";
+import {faChessRook, faGun} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {series_service} from "../../services/series.service";
 
 const Sidebar = () => {
-  const games = [
-    {
-      name: "lol",
-      display_name: "League of Legends",
-      icon: faChessRook,
-      series: [],
-    },
-    {
-      name: "valorant",
-      display_name: "Valorant",
-      icon: faGun,
-      series: [],
-    },
-  ];
 
-  const [activeGames, setActiveGames] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [games, setGames] = useState([])
+    const [activeGames, setActiveGames] = useState([]);
 
-  const handleButtonClick = async (index) => {
-    if (activeGames.includes(index)) {
-      // Si l'index est déjà dans le tableau, on le retire pour masquer la liste
-      setActiveGames(activeGames.filter((gameIndex) => gameIndex !== index));
-    } else {
-      // Sinon, on ajoute l'index pour afficher la liste
-      setActiveGames([...activeGames, index]);
-      //   setSelectedGame(games[index].name.toLowerCase());
+    const fetch_all_data = async () => {
+        const games = [
+            {
+                name: "lol",
+                display_name: "League of Legends",
+                icon: faChessRook,
+                series: [],
+            },
+            {
+                name: "valorant",
+                display_name: "Valorant",
+                icon: faGun,
+                series: [],
+            },
+        ];
+
+        const seriesPromises = games.map((game) =>
+            series_service
+                .getSerieByGame(game.name)
+                .then((response) => {
+                    const data = response.data.slice(-5);
+                    game.series.push(data);
+                })
+                .catch((error) => {
+                    console.error("Erreur lors de la récupération des séries:", error);
+                })
+        );
+
+        await Promise.all(seriesPromises);
+        setLoading(false);
+        return games;
     }
-  };
 
-  useEffect(() => {
-    games.forEach((game) => {
-      series_service
-        .getSerieByGame(game.name)
-        .then((response) => {
-          const data = response.data.slice(-5);
-          game.series.push(data);
-        })
-        .catch((error) => {
-          console.error("Erreur lors de la récupération des séries :", error);
-        });
-    });
-    console.log(games);
-  }, []);
+    useEffect(() => {
+        const fetchData = async () => {
+            const games = await fetch_all_data();
+            setGames(games);
+        }
 
-  return (
-    <div className="bg-white min-h-screen border border-cyan-950 sticky top-2 self-start p-4 rounded">
-      <h2 className="text-lg font-semibold pb-2">Sports et Compétitions</h2>
-      <ul>
-        {games.map((game, index) => (
-          <li className="py-1" key={index}>
-            <a
-              className="flex bg-slate-200 rounded-md shadow-sm items-center py-2 pl-1"
-              href={"/" + game.name.toLowerCase()}
-              onClick={(event) => {
-                event.preventDefault();
-                handleButtonClick(index);
-              }}
-            >
-              <FontAwesomeIcon icon={game.icon} />
-              <span className="ml-1">{game.display_name}</span>
-            </a>
-            {activeGames.includes(index) && (
-              <ul className="pl-8">
-                {game.series.map((serie, index) => (
-                  <div>
-                    <p>{console.log(serie)}</p>
-                    <li key={index}>
-                      <a href="/">
-                        {serie.leagueId.name + " " + serie.fullName}
-                      </a>
-                    </li>
-                  </div>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+        fetchData();
+    }, [])
+
+
+    const handleButtonClick = (index) => {
+        if (activeGames.includes(index)) {
+            setActiveGames(activeGames.filter((gameIndex) => gameIndex !== index));
+        } else {
+            setActiveGames([...activeGames, index]);
+        }
+    };
+
+    if (loading) {
+        return (
+            <div className="bg-white min-h-screen border border-cyan-950 sticky top-2 self-start p-4 rounded">
+                <h2 className="text-lg font-semibold pb-2">Sports et Compétitions</h2>
+                <div>Chargement...</div>
+            </div>)
+    } else {
+        return (
+            <div className="bg-white min-h-screen border border-cyan-950 sticky top-2 self-start p-4 rounded">
+                <h2 className="text-lg font-semibold pb-2">Sports et Compétitions</h2>
+                <ul>
+                    {games.map((game, index) => (
+                        <li className="py-1" key={index}>
+                            <a
+                                className="flex   bg-slate-200 rounded-md shadow-sm items-center py-2 pl-1"
+                                href={"/" + game.display_name}
+                                onClick={(event) => {
+                                    event.preventDefault();
+                                    handleButtonClick(index);
+                                }}
+                            >
+                                <FontAwesomeIcon icon={game.icon}/>
+                                <span className="ml-1">{game.display_name}</span>
+                            </a>
+                            {activeGames.includes(index) && (
+                                <ul className=" pl-8">
+                                    {game.series[0].map((serie, index) => {
+                                        return (<li key={index}>
+                                            <a href="/">{serie["leagueId"]["name"] + " " + serie["fullName"]}</a>
+                                        </li>)
+                                    })}
+                                </ul>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 };
 
 export default Sidebar;
+
