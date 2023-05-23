b9635c5bb46126b8615a674f037bcef9db40c2ec
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-05-24 01:00:08 +0200

    🎨 Ajout du composant Home - groupe de Composants

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Betlist from "./components/betlist/Betlist";
-import Cart from "./components/cart/Cart";
+
 import Navbar from "./components/navbar/Navbar";
-import Sidebar from "./components/sidebar/Sidebar";
 import Login from "./components/authentication/login/Login";
 import Signin from "./components/authentication/signin/Signin";
 import Logout from "./components/authentication/logout/Logout";
-import React from "react";
+
 import Complist from "./components/competitions/Complist";
+import Homepage from "./components/home/Homepage";
+
 
 export default function App() {
 	return (
@@ -26,10 +27,8 @@ export default function App() {
 									<Route
 										index
 										element={
-											<div className="grid grid-cols-[1fr_3fr_1fr] m-6 gap-5">
-												<Sidebar />
-												<Betlist />
-												<Cart />
+											<div>
+												<Homepage />
 											</div>
 										}
 									/>
