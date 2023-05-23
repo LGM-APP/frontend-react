ac52a2938af0fb25b6615e7a9d621b14dc6805ba
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-05-23 20:08:51 +0200

    ✨ Pagination ⚡️🎨 Split en sous-composants

--- src/components/competitions/Complist.js
+++ src/components/competitions/Complist.js
@@ -1,58 +1,35 @@
 import React from "react";
 import ComplistTable from "./ComplistTable";
 import Filter from "./Filter";
+import { useEffect, useState } from "react";
+import { comp_service } from "../../services/comp.service";
 
 const Complist = () => {
-	const tableItems = [
-		{
-			avatar:
-				"https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
-			name: "Liam James",
-			email: "liamjames@example.com",
-			phone_nimber: "+1 (555) 000-000",
-			position: "Software engineer",
-			salary: "$100K",
-		},
-		{
-			avatar: "https://randomuser.me/api/portraits/men/86.jpg",
-			name: "Olivia Emma",
-			email: "oliviaemma@example.com",
-			phone_nimber: "+1 (555) 000-000",
-			position: "Product designer",
-			salary: "$90K",
-		},
-		{
-			avatar: "https://randomuser.me/api/portraits/women/79.jpg",
-			name: "William Benjamin",
-			email: "william.benjamin@example.com",
-			phone_nimber: "+1 (555) 000-000",
-			position: "Front-end developer",
-			salary: "$80K",
-		},
-		{
-			avatar: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
-			name: "Henry Theodore",
-			email: "henrytheodore@example.com",
-			phone_nimber: "+1 (555) 000-000",
-			position: "Laravel engineer",
-			salary: "$120K",
-		},
-		{
-			avatar:
-				"https://images.unsplash.com/photo-1439911767590-c724b615299d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
-			name: "Amelia Elijah",
-			email: "amelia.elijah@example.com",
-			phone_nimber: "+1 (555) 000-000",
-			position: "Open source manager",
-			salary: "$75K",
-		},
-	];
+	const [tableItems, setTableItems] = useState([]);
+
+	useEffect(() => {
+		const fetchTournaments = async () => {
+			try {
+				const games = ["valorant", "lol"];
+				const data = await comp_service.getCompetitionsByGames(games);
+				setTableItems(data);
+				console.log(data);
+			} catch (error) {
+				console.error(
+					"Une erreur s'est produite lors de la récupération des compétitions :",
+					error
+				);
+			}
+		};
+
+		fetchTournaments();
+	}, []);
 	return (
 		<div className="flex flex-col m-auto w-[70%] mt-6">
 			<h2 className="text-gray-800 font-bold text-4xl">Compétitions</h2>
 			<div className="flex justify-center px-4 pb-4 gap-x-[5vh] ">
 				<ComplistTable tableItems={tableItems} />
-                <Filter />
+				<Filter />
 			</div>
 		</div>
 	);
--- src/components/competitions/ComplistTable.js
+++ src/components/competitions/ComplistTable.js
@@ -1,50 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { truncateText } from "../../utils/utils";
+import Pagination from '../pagination/Pagination'; // Import du composant de pagination
 
-const ComplistTable = ({tableItems}) => {
-	return (
-		<div className="mt-8 w-full bg-gray-50 shadow-sm rounded-lg overflow-x-auto">
-			<table className="w-full table-auto text-sm text-left">
-				<thead className="text-gray-600 font-medium border-b">
-					<tr>
-						<th className="py-3 px-6">Compétition</th>
-						<th className="py-3 px-6">Split</th>
-						<th className="py-3 px-6">Format</th>
-						<th className="py-3 px-6">Statut</th>
-					</tr>
-				</thead>
-				<tbody className="text-gray-600 divide-y">
-					{tableItems.map((item, idx) => (
-						<tr key={idx}>
-							<td className="flex items-center gap-x-3 py-3 px-6 whitespace-nowrap">
-								<img
-									src={item.avatar}
-									className="w-10 h-10 rounded-full"
-									alt="avatar"
-								/>
-								<div>
-									<span className="block text-gray-700 text-sm font-medium">
-										{item.name}
-									</span>
-									<span className="block text-gray-700 text-xs">
-										{item.email}
-									</span>
-								</div>
-							</td>
-							<td className="px-6 py-4 whitespace-nowrap">
-								{item.phone_nimber}
-							</td>
-							<td className="px-6 py-4 whitespace-nowrap">
-								{item.position}
-							</td>
-							<td className="px-6 py-4 whitespace-nowrap">
-								{item.salary}
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
-	);
+const ITEMS_PER_PAGE = 10; // Nombre d'éléments à afficher par page
+
+const ComplistTable = ({ tableItems }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Calcul du nombre total de pages
+  const totalPages = Math.ceil(tableItems.length / ITEMS_PER_PAGE);
+
+  // Calcul de l'indice de début et de fin pour les éléments de la page actuelle
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+
+  // Récupération des éléments de la page actuelle
+  const currentItems = tableItems.slice(startIndex, endIndex);
+
+  return (
+    <div className="mt-8 w-full bg-gray-50 shadow-sm rounded-lg overflow-x-auto">
+      <table className="w-full table-auto text-sm text-left">
+        <thead className="text-gray-600 font-medium border-b">
+          <tr>
+            <th className="py-3 px-6">Compétition</th>
+            <th className="py-3 px-6">Split</th>
+            <th className="py-3 px-6">Format</th>
+            <th className="py-3 px-6">Statut</th>
+          </tr>
+        </thead>
+        <tbody className="text-gray-600 divide-y">
+          {currentItems.map((item, idx) => (
+            <tr key={idx}>
+              <td className="flex items-center gap-x-3 py-3 px-6">
+                <div className="w-[15%] border-pink-400 bg-cover bg-no-repeat bg-center mr-2">
+                  <img
+                    src={item["serie"]["leagueId"]["imageUrl"]}
+                    alt="avatar"
+                  />
+                </div>
+                <div>
+                  <span className="block text-gray-700 text-sm font-medium truncate">
+                    {truncateText(
+                      item["serie"]["leagueId"]["name"],
+                      20
+                    )}
+                  </span>
+                </div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                {truncateText(item["serie"]["fullName"], 20)}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                {truncateText(item.name, 20)}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                {truncateText(item["winner"]["name"], 25)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination 
+        currentPage={currentPage} 
+        totalPages={totalPages} 
+        onPageChange={setCurrentPage} 
+      />
+    </div>
+  );
 };
 
 export default ComplistTable;
