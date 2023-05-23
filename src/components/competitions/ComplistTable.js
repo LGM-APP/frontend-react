30ee5a23ba5aa5572dcba9915de70b17bc8030d4
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-05-24 00:09:00 +0200

    💩 À revoir niveau comportement du tableau (w/h)

--- src/components/competitions/ComplistTable.js
+++ src/components/competitions/ComplistTable.js
@@ -1,95 +1,94 @@
 import React, { useState } from "react";
 import { truncateText } from "../../utils/utils";
-import Pagination from '../pagination/Pagination';
-import Loader from "../sidebar/loader/Loader";
+import Pagination from "../pagination/Pagination";
+import Loader from "../loader/Loader";
 
 const ITEMS_PER_PAGE = 5;
 
 const ComplistTable = ({ tableItems }) => {
-  const [currentPage, setCurrentPage] = useState(1);
+	const [currentPage, setCurrentPage] = useState(1);
 
-  const totalPages = Math.ceil(tableItems.length / ITEMS_PER_PAGE);
+	const totalPages = Math.ceil(tableItems.length / ITEMS_PER_PAGE);
 
+	const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+	const endIndex = startIndex + ITEMS_PER_PAGE;
 
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const endIndex = startIndex + ITEMS_PER_PAGE;
+	const currentItems = tableItems.slice(startIndex, endIndex);
 
-  const currentItems = tableItems.slice(startIndex, endIndex);
+	if (tableItems.length === 0) {
+		return (
+			<div className="mt-8 w-full bg-gray-50 shadow-sm rounded-lg overflow-x-auto">
+				<table className="w-full table-auto text-sm text-left">
+					<thead className="text-gray-600 font-medium border-b">
+						<tr>
+							<th className="py-3 px-6">Compétition</th>
+							<th className="py-3 px-6">Split</th>
+							<th className="py-3 px-6">Format</th>
+							<th className="py-3 px-6">Statut</th>
+						</tr>
+					</thead>
+					<tbody className="text-gray-600 divide-y">
+						<tr>
+							<td className="pb-2" colSpan="4">
+								<Loader />
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 
-  if (tableItems.length === 0) {
-    return (
-      <div className="mt-8 w-full bg-gray-50 shadow-sm rounded-lg overflow-x-auto">
-        <table className="w-full table-auto text-sm text-left">
-          <thead className="text-gray-600 font-medium border-b">
-            <tr>
-              <th className="py-3 px-6">Compétition</th>
-              <th className="py-3 px-6">Split</th>
-              <th className="py-3 px-6">Format</th>
-              <th className="py-3 px-6">Statut</th>
-            </tr>
-          </thead>
-          <tbody className="text-gray-600 divide-y">
-            <tr>
-              <td className="p-2" colSpan="4">
-                <Loader />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-
-  return (
-    <div className="mt-8 w-full bg-gray-50 shadow-sm rounded-lg overflow-x-auto">
-      <table className="w-full table-auto text-sm text-left">
-        <thead className="text-gray-600 font-medium border-b">
-          <tr>
-            <th className="py-3 px-6">Compétition</th>
-            <th className="py-3 px-6">Split</th>
-            <th className="py-3 px-6">Format</th>
-            <th className="py-3 px-6">Statut</th>
-          </tr>
-        </thead>
-        <tbody className="text-gray-600 divide-y">
-          {currentItems.map((item, idx) => (
-            <tr key={idx}>
-              <td className="flex items-center gap-x-3 py-3 px-6">
-                <div className="w-[15%] border-pink-400 bg-cover bg-no-repeat bg-center mr-2">
-                  <img
-                    src={item["serie"]["leagueId"]["imageUrl"]}
-                    alt="avatar"
-                  />
-                </div>
-                <div>
-                  <span className="block text-gray-700 text-sm font-medium truncate">
-                    {truncateText(
-                      item["serie"]["leagueId"]["name"],
-                      20
-                    )}
-                  </span>
-                </div>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {truncateText(item["serie"]["fullName"], 20)}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {truncateText(item.name, 20)}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {truncateText(item["winner"]["name"], 25)}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <Pagination 
-        currentPage={currentPage} 
-        totalPages={totalPages} 
-        onPageChange={setCurrentPage} 
-      />
-    </div>
-  );
+	return (
+		<div className="mt-8 w-full bg-gray-50 shadow-sm rounded-lg overflow-x-auto">
+			<table className="w-full h-72 table-auto text-sm text-left">
+				<thead className="text-gray-600 font-medium border-b">
+					<tr>
+						<th className="py-3 px-6">Compétition</th>
+						<th className="py-3 px-6">Split</th>
+						<th className="py-3 px-6">Format</th>
+						<th className="py-3 px-6">Statut</th>
+					</tr>
+				</thead>
+				<tbody className="text-gray-600 divide-y space-y-4">
+					{currentItems.map((item, idx) => (
+						<tr key={idx}>
+							<td className="flex items-center gap-x-2 py-3 px-4">
+								<div className="w-[15%] border-pink-400 mr-2">
+									<img
+										src={item["serie"]["leagueId"]["imageUrl"]}
+										alt="avatar"
+									/>
+								</div>
+								<div>
+									<span className="block text-gray-700 text-sm font-medium">
+										{truncateText(
+											item["serie"]["leagueId"]["name"],
+											20
+										)}
+									</span>
+								</div>
+							</td>
+							<td className="px-8 py-4 whitespace-nowrap">
+								{truncateText(item["serie"]["fullName"], 20)}
+							</td>
+							<td className="px-8 py-4 ">
+								{truncateText(item.name, 20)}
+							</td>
+							<td className="px-8 py-4 whitespace-nowrap">
+								{truncateText(item["winner"]["name"], 25)}
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+			<Pagination
+				currentPage={currentPage}
+				totalPages={totalPages}
+				onPageChange={setCurrentPage}
+			/>
+		</div>
+	);
 };
 
 export default ComplistTable;
