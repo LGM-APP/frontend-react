8bb3dfc7d94a207551ab4bb7764b7a120762c674
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-05-24 01:18:20 +0200

    💄 Retouche de style (Filter a une taille fixe)

--- src/components/competitions/Filter.js
+++ src/components/competitions/Filter.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Filter = () => {
-	return (
-		<div className="bg-gray-50 border-2 w-80 mt-8 shadow-sm rounded-lg">
-			<div className="py-3 px-4">
-				<h2 className="text-gray-600 font-semibold pb-1.5 border-b">
-					Filtres
-				</h2>
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-gray-50 border-2 w-[20%] mt-8 shadow-sm rounded-lg flex-shrink-0">
+      <div className="py-3 px-4">
+        <h2 className="text-gray-600 font-semibold pb-1.5 border-b">
+          Filtres
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
