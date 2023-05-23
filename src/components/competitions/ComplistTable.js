ec404f55ebfc7ce1c580a771e00e5596d325921d
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-05-23 21:06:58 +0200

    ✨ Ajout d'un Loader

--- src/components/competitions/ComplistTable.js
+++ src/components/competitions/ComplistTable.js
@@ -1,21 +1,44 @@
 import React, { useState } from "react";
 import { truncateText } from "../../utils/utils";
-import Pagination from '../pagination/Pagination'; // Import du composant de pagination
+import Pagination from '../pagination/Pagination';
+import Loader from "../sidebar/loader/Loader";
 
-const ITEMS_PER_PAGE = 10; // Nombre d'éléments à afficher par page
+const ITEMS_PER_PAGE = 5;
 
 const ComplistTable = ({ tableItems }) => {
   const [currentPage, setCurrentPage] = useState(1);
 
-  // Calcul du nombre total de pages
   const totalPages = Math.ceil(tableItems.length / ITEMS_PER_PAGE);
 
-  // Calcul de l'indice de début et de fin pour les éléments de la page actuelle
+
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
 
-  // Récupération des éléments de la page actuelle
   const currentItems = tableItems.slice(startIndex, endIndex);
+
+  if (tableItems.length === 0) {
+    return (
+      <div className="mt-8 w-full bg-gray-50 shadow-sm rounded-lg overflow-x-auto">
+        <table className="w-full table-auto text-sm text-left">
+          <thead className="text-gray-600 font-medium border-b">
+            <tr>
+              <th className="py-3 px-6">Compétition</th>
+              <th className="py-3 px-6">Split</th>
+              <th className="py-3 px-6">Format</th>
+              <th className="py-3 px-6">Statut</th>
+            </tr>
+          </thead>
+          <tbody className="text-gray-600 divide-y">
+            <tr>
+              <td className="p-2" colSpan="4">
+                <Loader />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 
   return (
     <div className="mt-8 w-full bg-gray-50 shadow-sm rounded-lg overflow-x-auto">
