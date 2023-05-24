aba8926927dad87daefe9e9697be046d18180af5
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-05-24 19:24:04 +0200

    ♻️ Déplacement de getCompetitionsByGames()

--- src/services/comp.service.js
+++ src/services/comp.service.js
@@ -1,19 +1,23 @@
 import Axios from "./api.service";
 
 const getCompetitionByGame = (game) => {
-  return Axios.get(`/tournaments/${game}`)
-    .then(response => response.data)
-    .catch(error => {
-      console.error("Une erreur s'est produite lors de la récupération des compétitions :", error);
-      throw error;
-    });
+	return Axios.get(`/tournaments/${game}`)
+		.then((response) => response.data)
+		.catch((error) => {
+			console.error(
+				"Une erreur s'est produite lors de la récupération des compétitions :",
+				error
+			);
+			throw error;
+		});
+};
+
+const getCompetitionsByGames = async (games) => {
+	const promises = games.map((game) => getCompetitionByGame(game));
+	const results = await Promise.all(promises);
+	return results.flat();
 };
 
 export const comp_service = {
-  getCompetitionByGame,
-  getCompetitionsByGames: async (games) => {
-    const promises = games.map(game => getCompetitionByGame(game));
-    const results = await Promise.all(promises);
-    return results.flat();
-  }
+	getCompetitionByGame,	getCompetitionsByGames,
 };
