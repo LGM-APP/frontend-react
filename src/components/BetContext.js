728af6e1a6876b71a418784c38f1900c9a23a8ce
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-06-02 20:43:53 +0200

    👽️ Ajout du stake au BetContext

--- src/components/BetContext.js
+++ src/components/BetContext.js
@@ -5,9 +5,10 @@ export const BetContext = createContext();
 export const BetProvider = ({ children }) => {
   const [bets, setBets] = useState([]);
   const [totalOdds, setTotalOdds] = useState(1);
+  const [stake, setStake] = useState(0);
 
   return (
-    <BetContext.Provider value={{ bets, setBets, totalOdds, setTotalOdds }}>
+    <BetContext.Provider value={{ bets, setBets, totalOdds, setTotalOdds, stake, setStake }}>
       {children}
     </BetContext.Provider>
   );
