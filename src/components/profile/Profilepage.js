6c0711717d5207b44151664aff256b2381ad7f08
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-06-02 20:41:34 +0200

    ✨ Ajout du composant Profilepage

---
+++ src/components/profile/Profilepage.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { bet_service } from "../../services/bet.service";
+import { user_service } from "../../services/user.service";
+
+const Profilepage = () => {
+	const [userData, setUserData] = useState(null);
+	const [betList, setBetList] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const userData = await user_service.getUserData();
+				setUserData(userData);
+
+				const betList = await bet_service.getBet();
+				setBetList(betList);
+			} catch (error) {
+				console.error(
+					"Erreur lors de la récupération des données :",
+					error
+				);
+			}
+		};
+		fetchData();
+	}, []);
+
+	return (
+		<div className="flex">
+			<div className="w-1/2 bg-gray-200 p-4">
+				<h2 className="text-2xl font-semibold mb-4">
+					Informations utilisateur
+				</h2>
+				{userData ? (
+					<div>
+						<p>
+							<span className="font-semibold">Nom :</span>{" "}
+							{userData.firstName} {userData.lastName}
+						</p>
+						<p>
+							<span className="font-semibold">Email :</span>{" "}
+							{userData.email}
+						</p>
+						<p>
+							<span className="font-semibold">Points :</span>{" "}
+							{userData.point}
+						</p>
+					</div>
+				) : (
+					<p>Chargement des données utilisateur...</p>
+				)}
+			</div>
+			<div className="w-1/2 bg-gray-100 p-4">
+				<h2 className="text-2xl font-semibold mb-4">Liste des paris</h2>
+				{betList.length > 0 ? (
+					<ul>
+						{betList.map((bet) => (
+							<li key={bet.id}>
+								<p>
+									<span className="font-semibold">Match ID :</span>{" "}
+									{bet.matchId}
+								</p>
+								<p>
+									<span className="font-semibold">Bet Team ID :</span>{" "}
+									{bet.betTeamId}
+								</p>
+								<p>
+									<span className="font-semibold">Montant :</span>{" "}
+									{bet.amount}
+								</p>
+								<p>
+									<span className="font-semibold">Côte :</span>{" "}
+									{bet.odd}
+								</p>
+							</li>
+						))}
+					</ul>
+				) : (
+					<p>Aucun pari trouvé.</p>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default Profilepage;
