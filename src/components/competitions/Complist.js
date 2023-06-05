8d5e8afd9e8c7406b073a1da7040fa558956b85f
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-06-05 20:21:45 +0200

    💄 Modification de l'affichage des series

--- src/components/competitions/Complist.js
+++ src/components/competitions/Complist.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { comp_service } from "../../services/comp.service";
+import ComplistTable from "./ComplistTable";
+import Pagination from "../pagination/Pagination";
 
 const Complist = () => {
   const [compData, setCompData] = useState({ series: [], totalPages: 0 });
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await comp_service.getAllCompData(1);
+        const data = await comp_service.getAllCompData(currentPage);
         setCompData(data);
       } catch (error) {
         console.error("Erreur lors de la récupération des données d'équipe :", error);
@@ -16,22 +19,18 @@ const Complist = () => {
     };
 
     fetchData();
-  }, []);
+  }, [currentPage]);
 
   return (
     <div>
-      <h1>Informations de la route</h1>
-      {compData.series.map((serie) => (
-        <div key={serie.id}>
-          <h2>Nom de la série : {serie.fullName}</h2>
-          <p>Date de début : {serie.beginAt}</p>
-          <p>Date de fin : {serie.endAt}</p>
-          <hr />
-        </div>
-      ))}
+      <h1 className="text-6xl">Informations de la route</h1>
+      <ComplistTable series={compData.series} />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={compData.totalPages}
+        onPageChange={setCurrentPage}
+      />
     </div>
-
-	
   );
 };
 
--- src/components/competitions/ComplistTable.js
+++ src/components/competitions/ComplistTable.js
@@ -1,94 +1,52 @@
-import React, { useState } from "react";
-import { truncateText } from "../../utils/utils";
-import Pagination from "../pagination/Pagination";
-import Loader from "../loader/Loader";
+import React from "react";
 
-const ITEMS_PER_PAGE = 5;
-
-const ComplistTable = ({ tableItems }) => {
-	const [currentPage, setCurrentPage] = useState(1);
-
-	const totalPages = Math.ceil(tableItems.length / ITEMS_PER_PAGE);
-
-	const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-	const endIndex = startIndex + ITEMS_PER_PAGE;
-
-	const currentItems = tableItems.slice(startIndex, endIndex);
-
-	if (tableItems.length === 0) {
-		return (
-			<div className="mt-8 w-full bg-gray-50 shadow-sm rounded-lg overflow-x-auto">
-				<table className="w-full table-auto text-lg text-left">
-					<thead className="text-gray-600 font-medium border-b">
-						<tr>
-							<th className="py-3 px-6">Compétition</th>
-							<th className="py-3 px-6">Split</th>
-							<th className="py-3 px-6">Format</th>
-							<th className="py-3 px-6">Statut</th>
-						</tr>
-					</thead>
-					<tbody className="text-gray-600 divide-y">
-						<tr>
-							<td className="pb-2" colSpan="4">
-								<Loader />
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		);
-	}
-
-	return (
-		<div className="mt-8 bg-gray-50 w-screen border border-black shadow-sm rounded-lg overflow-x-auto">
-			<table className="w-full table-fixed text-sm text-left">
-				<thead className="text-gray-600 font-medium border-b">
-					<tr>
-						<th className="py-3 px-6 w-1/4">Compétition</th>
-						<th className="py-3 px-6 w-1/3">Split</th>
-						<th className="py-3 px-6 w-1/6">Format</th>
-						<th className="py-3 px-6 w-1/5">Statut</th>
-					</tr>
-				</thead>
-				<tbody className="text-gray-600 divide-y space-y-4">
-					{currentItems.map((item, idx) => (
-						<tr key={idx}>
-							<td className="flex items-center gap-x-2 py-3 px-4">
-								<div className="w-[10vh] flex items-center aspect-[4/3] object-contain border-pink-400 mr-2">
-									<img
-										src={item["serie"]["leagueId"]["imageUrl"]}
-										alt="serie logo"
-									/>
-								</div>
-								<div>
-									<span className="block text-gray-700 text-sm font-medium whitespace-nowrap">
-										{truncateText(
-											item["serie"]["leagueId"]["name"],
-											20
-										)}
-									</span>
-								</div>
-							</td>
-							<td className="px-8 py-4 whitespace-nowrap">
-								{item["serie"]["fullName"]}
-							</td>
-							<td className="px-8 py-4 whitespace-nowrap">
-								{truncateText(item.name, 20)}
-							</td>
-							<td className="px-8 py-4 whitespace-nowrap">
-								{truncateText(item["winner"]["name"], 25)}
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-			<Pagination
-				currentPage={currentPage}
-				totalPages={totalPages}
-				onPageChange={setCurrentPage}
-			/>
-		</div>
-	);
+const ComplistTable = ({ series }) => {
+  return (
+    <div className="m-8 bg-gray-50 w-3/4 mx-auto border border-black shadow-sm rounded-lg overflow-x-auto">
+      <table className="w-full table-fixed text-sm text-left">
+        <thead className="text-gray-600 font-medium border-b">
+          <tr>
+            <th className="py-2 px-4 w-1/4">Compétition</th>
+            <th className="py-2 px-4 w-1/4">Split</th>
+            <th className="py-2 px-4 w-1/4">Début</th>
+            <th className="py-2 px-4 w-1/4">Fin</th>
+          </tr>
+        </thead>
+        <tbody className="text-gray-600 divide-y space-y-2">
+          {series.map((serie) => (
+            <tr key={serie.id}>
+              <td className="flex items-center gap-x-2 py-2 px-4">
+                <div className="w-16 h-16 flex items-center justify-center border-pink-400 mr-2">
+                  <img
+                    className="w-16 h-16 object-contain"
+                    src={serie.leagueId.imageUrl}
+                    alt="logo de la série"
+                  />
+                </div>
+                <div>
+                  <span className="block text-gray-700 text-xs font-medium whitespace-nowrap">
+                    {serie.leagueId.name}
+                  </span>
+                  <span className="block text-gray-500 text-xs font-medium whitespace-nowrap">
+                    {serie.leagueId.videoGame.name}
+                  </span>
+                </div>
+              </td>
+              <td className="py-2 px-4 whitespace-nowrap">
+                {serie.fullName}
+              </td>
+              <td className="py-2 px-4 whitespace-nowrap">
+                {serie.beginAt}
+              </td>
+              <td className="py-2 px-4 whitespace-nowrap">
+                {serie.endAt}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ComplistTable;
