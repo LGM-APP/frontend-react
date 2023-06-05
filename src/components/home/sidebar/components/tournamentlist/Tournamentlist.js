a260dea41fdfbc8578a78bc7ed529036f11335ae
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-06-05 20:49:26 +0200

    💄 Changement affichage des détails d'un tournoi

--- src/components/home/sidebar/components/tournamentlist/Tournamentlist.js
+++ src/components/home/sidebar/components/tournamentlist/Tournamentlist.js
@@ -15,6 +15,7 @@ const Tournamentlist = () => {
                     const matchsData = await matchs_service.getMatchsByTournamentId(tournament.id);
                     return {
                         id: tournament.id,
+                        logo: tournament.serie.leagueId.imageUrl,
                         name: tournament.name,
                         league_name: tournament.serie.leagueId.name + " " + tournament.serie.fullName,
                         begin_at: tournament.beginAt,
@@ -23,7 +24,6 @@ const Tournamentlist = () => {
                     };
                 })
             );
-            console.log(tournamentsData);
             setTournaments(tournamentsData);
         };
 
@@ -31,21 +31,36 @@ const Tournamentlist = () => {
     }, [id]);
 
     return (
-        <div>
-            <h1>Tournament List</h1>
-            {tournaments.map((tournament) => (
-                <div key={tournament.id}>
-                    <h2>{tournament.name}</h2>
-                    <h3>{tournament.league_name}</h3>
-                    <h3>{tournament.begin_at}</h3>
-                    <h3>{tournament.end_at}</h3>
-                    <ul>
-                        {tournament.matchs.map((match) => (
-                            <li key={match.id}>{match.name}</li>
+        <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+            <div className="max-w-lg">
+                {tournaments.map((tournament) => (
+                    <div key={tournament.id} className="my-4">
+                        <h1 className="text-gray-800 text-xl font-bold sm:text-2xl mb-2">{tournament.league_name}</h1>
+                        <img className="w-32 h-32" src={tournament.logo} alt="Tournament logo" />
+                        <h3 className="text-md mb-2">{tournament.name}</h3>
+                        <h3 className="text-md mb-2">Date de début : {tournament.begin_at}</h3>
+                        <h3 className="text-md mb-4">Date de fin : {tournament.end_at}</h3>
+                    </div>
+                ))}
+            </div>
+            <div className="mt-12 shadow-sm border rounded-lg overflow-x-auto">
+                <table className="w-full table-auto text-sm text-left">
+                    <thead className="bg-gray-50 text-gray-600 font-medium border-b">
+                        <tr>
+                            <th className="py-3 px-6 text-center">Liste des matchs</th>
+                        </tr>
+                    </thead>
+                    <tbody className="text-gray-600 bg-gray-100 divide-y">
+                        {tournaments.map((tournament) => (
+                            tournament.matchs.map((match) => (
+                                <tr key={match.id}>
+                                    <td className="px-6 py-4 whitespace-nowrap">{match.name}</td>
+                                </tr>
+                            ))
                         ))}
-                    </ul>
-                </div>
-            ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
