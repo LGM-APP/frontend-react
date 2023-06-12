604554e1d0894e3cf9141ab11b5b1a5b3484c034
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-06-12 22:41:46 +0200

    💄 Retouches de style

--- src/components/games/clicu/Clicu.js
+++ src/components/games/clicu/Clicu.js
@@ -1,119 +1,125 @@
-import React, { useEffect } from 'react';
-import './Clicu.css';
+import React, { useEffect } from "react";
+import "./Clicu.css";
 
 const Clicu = () => {
-  const gameWidth = 770;
-  const gameHeight = 770;
-  const gameDuration = 30;
-  let timeLeft = gameDuration;
-  let score = 0;
-  let timerId = null;
+	const gameWidth = 770;
+	const gameHeight = 770;
+	const gameDuration = 30;
+	let timeLeft = gameDuration;
+	let score = 0;
+	let timerId = null;
 
-  useEffect(() => {
-    const gameContainer = document.getElementById('game-container');
+	useEffect(() => {
+		const gameContainer = document.getElementById("game-container");
 
-    function generateNewTargets() {
-      if (timeLeft > 0) {
-        const position = getRandomPosition();
-        const target = createTarget(position);
-        gameContainer.appendChild(target);
-      }
-    }
+		function generateNewTargets() {
+			if (timeLeft > 0) {
+				const position = getRandomPosition();
+				const target = createTarget(position);
+				gameContainer.appendChild(target);
+			}
+		}
 
-    function getRandomPosition() {
-      const x = Math.floor(Math.random() * gameWidth);
-      const y = Math.floor(Math.random() * gameHeight);
-      return { x, y };
-    }
+		function getRandomPosition() {
+			const x = Math.floor(Math.random() * gameWidth);
+			const y = Math.floor(Math.random() * gameHeight);
+			return { x, y };
+		}
 
-    function createTarget(position) {
-      const target = document.createElement('div');
-      target.className = 'target';
-      target.style.left = `${position.x}px`;
-      target.style.top = `${position.y}px`;
+		function createTarget(position) {
+			const target = document.createElement("div");
+			target.className = "target";
+			target.style.left = `${position.x}px`;
+			target.style.top = `${position.y}px`;
 
-      target.addEventListener('click', () => {
-        score += 300;
-        target.remove();
-        updateScore();
-      });
+			target.addEventListener("click", () => {
+				score += 300;
+				target.remove();
+				updateScore();
+			});
 
-      return target;
-    }
+			return target;
+		}
 
-    function updateScore() {
-      const scoreElement = document.getElementById('score');
-      scoreElement.textContent = `Score: ${score}`;
-    }
+		function updateScore() {
+			const scoreElement = document.getElementById("score");
+			scoreElement.textContent = `Score: ${score}`;
+		}
 
-    function updateTime() {
-      const timeElement = document.getElementById('time');
-      timeElement.textContent = `Time: ${timeLeft}s`;
-    }
+		function updateTime() {
+			const timeElement = document.getElementById("time");
+			timeElement.textContent = `Time: ${timeLeft}s`;
+		}
 
-    function checkMissedTarget(event) {
-      const clickedElement = event.target;
+		function checkMissedTarget(event) {
+			const clickedElement = event.target;
 
-      if (clickedElement.className !== 'target') {
-        score -= 100;
-        updateScore();
-      }
-    }
+			if (clickedElement.className !== "target") {
+				score -= 100;
+				updateScore();
+			}
+		}
 
-    function startGame() {
-      score = 0;
-      timeLeft = gameDuration;
+		function startGame() {
+			score = 0;
+			timeLeft = gameDuration;
 
-      gameContainer.innerHTML = '';
+			gameContainer.innerHTML = "";
 
-      updateScore();
-      updateTime();
+			updateScore();
+			updateTime();
 
-      timerId = setInterval(() => {
-        timeLeft--;
-        updateTime();
+			timerId = setInterval(() => {
+				timeLeft--;
+				updateTime();
 
-        if (timeLeft === 0) {
-          endGame();
-        }
-      }, 1000);
+				if (timeLeft === 0) {
+					endGame();
+				}
+			}, 1000);
 
-      const numSpawn = 10;
-      const numEverySec = 2;
+			const numSpawn = 10;
+			const numEverySec = 2;
 
-      for (let i = 0; i < numSpawn; i++) {
-        generateNewTargets();
-      }
+			for (let i = 0; i < numSpawn; i++) {
+				generateNewTargets();
+			}
 
-      for (let i = 0; i < numEverySec; i++) {
-        setInterval(generateNewTargets, 1000);
-      }
+			for (let i = 0; i < numEverySec; i++) {
+				setInterval(generateNewTargets, 1000);
+			}
 
-      gameContainer.addEventListener('click', checkMissedTarget);
-    }
+			gameContainer.addEventListener("click", checkMissedTarget);
+		}
 
-    function endGame() {
-      clearInterval(timerId);
-      timerId = null;
-      gameContainer.removeEventListener('click', checkMissedTarget);
-      alert(`Game over! Your score is ${score}.`);
-    }
+		function endGame() {
+			clearInterval(timerId);
+			timerId = null;
+			gameContainer.removeEventListener("click", checkMissedTarget);
+			alert(`Game over! Your score is ${score}.`);
+		}
 
-    startGame();
+		startGame();
 
-    return () => {
-      clearInterval(timerId);
-      gameContainer.removeEventListener('click', checkMissedTarget);
-    };
-  }, []);
+		return () => {
+			clearInterval(timerId);
+			gameContainer.removeEventListener("click", checkMissedTarget);
+		};
+	}, []);
 
-  return (
-    <div>
-      <div id="game-container"></div>
-      <div id="score">Score: {score}</div>
-      <div id="time">Time: {timeLeft}s</div>
-    </div>
-  );
+	return (
+		<div className="grid justify-center items-center min-h-screen">
+			<div className="text-lg font-semibold">
+				<div id="score" className="game-info">
+					Score: {score}
+				</div>
+				<div id="time" className="game-info">
+					Time: {timeLeft}s
+				</div>
+				<div id="game-container" className="game-container"></div>
+			</div>
+		</div>
+	);
 };
 
 export default Clicu;
