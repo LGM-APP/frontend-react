88e515977a07f6d1f72eac60b81d1c66be66d88a
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-06-12 22:41:30 +0200

    🐛 Fonctionnement général fixé

--- src/components/games/quizu/Quizu.js
+++ src/components/games/quizu/Quizu.js
@@ -1,75 +1,90 @@
-import React, { useState } from 'react';
-import questionsData from './questionsData.json'; // Importez vos données de questions à partir d'un fichier JSON
-import './Quizu.css';
+import React, { useState, useEffect } from "react";
+import questionsData from "./questionsData.json";
 
-const Question = ({ questionObj, onQuestionAnswered }) => {
+const Quizu = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [score, setScore] = useState(0);
+  const [isQuizEnd, setIsQuizEnd] = useState(false);
 
-  const onAnswerClick = (answer) => {
+  const currentQuestion = questionsData[currentQuestionIndex];
+  
+  useEffect(() => {
+    if (selectedAnswer) {
+      if (selectedAnswer.isCorrect) setScore((prevScore) => prevScore + 1);
+
+      if (currentQuestionIndex < questionsData.length - 1) {
+        setTimeout(() => {
+          setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+        }, 1000); // Temps d'attente en millisecondes
+      } else {
+        setIsQuizEnd(true);
+      }
+      
+      // Clear selected answer
+      setTimeout(() => {
+        setSelectedAnswer(null);
+      }, 1000);
+    }
+  }, [selectedAnswer, currentQuestionIndex]);
+
+  const handleAnswerClick = (answer) => {
     setSelectedAnswer(answer);
-    onQuestionAnswered(answer);
   };
 
-  const createAnswer = (answer) => {
+  const restartQuiz = () => {
+    setCurrentQuestionIndex(0);
+    setScore(0);
+    setIsQuizEnd(false);
+  };
+
+  if (isQuizEnd) {
     return (
-      <button
-        key={answer.text}
-        className="answer"
-        onClick={() => onAnswerClick(answer)}
-      >
-        {answer.text}
-      </button>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h2 className="mb-4 text-2xl font-bold">
+          {score >= 7
+            ? `Félicitations ! Ton score est de ${score}/${questionsData.length} points.`
+            : `Dommage ! Ton score est de ${score}/${questionsData.length} points.`}
+        </h2>
+        <button
+          className="px-4 py-2 mt-4 text-white bg-cyan-950 rounded-full hover:bg-cyan-900"
+          onClick={restartQuiz}
+        >
+          Recommencer
+        </button>
+      </div>
     );
-  };
-
-  const { text, answers } = questionObj;
+  }
 
   return (
-    <div className="question">
-      <h2>{text}</h2>
-      <div className="answers">
-        {answers.map((answer) => createAnswer(answer))}
+    <div className="flex flex-col items-center mt-24 min-h-screen">
+      <h2 className="mb-4 text-2xl font-bold">{`Score: ${score}/${questionsData.length}`}</h2>
+      <div className="border-2 border-cyan-950 p-4 rounded">
+        <h2 className="mb-4 text-xl font-bold">{currentQuestion.text}</h2>
+        <div>
+          {currentQuestion.answers.map((answer, index) => (
+            <button
+              key={index}
+              className={`block w-full mb-4 p-4 rounded font-bold 
+                ${selectedAnswer 
+                  ? answer === selectedAnswer 
+                    ? answer.isCorrect 
+                      ? "bg-green-500 text-white" 
+                      : "bg-red-500 text-white"
+                    : answer.isCorrect
+                      ? "bg-green-500 text-white" 
+                      : "bg-blue-500 text-white hover:bg-blue-700"
+                  : "bg-blue-500 text-white hover:bg-blue-700"
+              }`}
+              onClick={() => handleAnswerClick(answer)}
+            >
+              {answer.text}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-const Quiz = () => {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [score, setScore] = useState(0);
-
-  const displayQuestion = () => {
-    return (
-      <Question
-        questionObj={questionsData[currentQuestionIndex]}
-        onQuestionAnswered={handleQuestionAnswered}
-      />
-    );
-  };
-
-  const handleQuestionAnswered = (answer) => {
-    if (answer.isCorrect) {
-      setScore((prevScore) => prevScore + 1);
-    }
-
-    setTimeout(() => {
-      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-    }, 1000);
-  };
-
-  if (currentQuestionIndex === questionsData.length) {
-    setTimeout(() => {
-      alert(`Quiz Fini!\nTon score est de : ${score}/${questionsData.length} points`);
-    }, 100);
-    return null;
-  }
-
-  return (
-    <div id="app-container">
-      <div id="score-container">Score: {score}/{questionsData.length}</div>
-      <div id="questions-container">{displayQuestion()}</div>
-    </div>
-  );
-};
-
-export default Quiz;
+export default Quizu;
